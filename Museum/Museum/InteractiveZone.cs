231a40593e184fb7fe6e5bce20b1a455dac32952
Nekekas <nikita.manichenko@mail.ru>
2024-06-12 15:00:26 +0700

    add project

---
+++ Museum/Museum/InteractiveZone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Maps;
+
+namespace Museum
+{
+    public class InteractiveZone
+    {
+        private Polygon _zone;
+        private string _songPath;
+    
+        public Polygon Zone => _zone;
+        public string SongPath => _songPath;
+
+        public InteractiveZone(Polygon zone, string songPath)
+        {
+            _zone = zone;
+            _songPath = songPath;
+        }
+
+        public bool InZone(Position position)
+        {
+            
+        }
+    }
+}
---
+++ Museum/Museum/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace Museum
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+
+            Polygon zone1 = new Polygon
+            {
+                StrokeColor = Color.Red,
+                StrokeWidth = 8,
+                FillColor = Color.FromRgba(252, 0, 0, 125),
+                Geopath =
+                {
+                    new Position(59.278476, 39.716340),
+                    new Position(59.278347, 39.716519),
+                    new Position(59.278435, 39.716891),
+                    new Position(59.278598, 39.716742)
+                }
+            };
+
+            map.MapElements.Add(zone1);
+        }
+    }
+}

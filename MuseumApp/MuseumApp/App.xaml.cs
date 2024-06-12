40002d4db5f3c4a545579974d24efd1dbda1a459
Nekekas <nikita.manichenko@mail.ru>
2024-06-12 14:27:41 +0700

    Add project

---
+++ MuseumApp/MuseumApp/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace MuseumApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
